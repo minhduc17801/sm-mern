da979afa93131ef132ef2281a420b18e354b7fcd
minhduc17801 <minhduc17801@gmail.com>
2023-04-29 22:53:21 +0700

    client: Profile page

--- client/src/pages/ProfilePage/index.jsx
+++ client/src/pages/ProfilePage/index.jsx
@@ -1,5 +1,61 @@
+import { useMediaQuery, Box } from '@mui/material';
+import Navbar from '../Navbar';
+import UserWidget from '../widgets/UserWidget';
+import FriendListWidget from '../widgets/FriendListWidget';
+import MyPostWidget from '../widgets/MyPostWidget';
+import PostsWidget from '../widgets/PostsWidget';
+import { useParams } from 'react-router-dom';
+import { useEffect, useState } from 'react';
+
 function ProfilePage() {
-    return <h1>Profile Page</h1>;
+    const [user, setUser] = useState(null);
+    const isNonMobile = useMediaQuery('(min-width: 768px)');
+    const { userId } = useParams();
+
+    useEffect(() => {
+        const getUser = async (userId) => {
+            try {
+                const res = await fetch(`http://localhost:3001/user/${userId}`);
+                const user = await res.json();
+                setUser(user);
+            } catch (error) {
+                console.log(error);
+            }
+        };
+
+        getUser(userId);
+    }, []);
+
+    if (!user) return null;
+    return (
+        <Box>
+            <Navbar />
+            <Box
+                width="100%"
+                padding="2rem 6%"
+                display={isNonMobile ? 'flex' : 'block'}
+                gap="2rem"
+                justifyContent="center"
+            >
+                <Box flexBasis={isNonMobile ? '26%' : undefined}>
+                    <UserWidget
+                        userId={userId}
+                        picturePath={user.picturePath}
+                    />
+                    <Box m="2rem 0" />
+                    <FriendListWidget userId={userId} />
+                </Box>
+                <Box
+                    flexBasis={isNonMobile ? '42%' : undefined}
+                    mt={isNonMobile ? undefined : '2rem'}
+                >
+                    <MyPostWidget picturePath={user.picturePath} />
+                    <Box m="2rem 0" />
+                    <PostsWidget userId={userId} isProfile />
+                </Box>
+            </Box>
+        </Box>
+    );
 }
 
 export default ProfilePage;
--- client/src/pages/widgets/PostsWidget.jsx
+++ client/src/pages/widgets/PostsWidget.jsx
@@ -2,7 +2,9 @@ import { useDispatch, useSelector } from 'react-redux';
 import Post from '../../components/Post';
 import { setPosts } from '../../state/reducer';
 import { useEffect } from 'react';
+import { Box, Typography, useTheme } from '@mui/material';
 const PostsWidget = ({ userId, isProfile = false }) => {
+    const { palette } = useTheme();
     const dispatch = useDispatch();
     const posts = useSelector((state) => state.posts);
     const getAllPosts = async () => {
@@ -40,6 +42,13 @@ const PostsWidget = ({ userId, isProfile = false }) => {
             {posts.map((post, index) => (
                 <Post key={index} {...post} />
             ))}
+            {posts.length === 0 && (
+                <Box textAlign="center">
+                    <Typography variant="h1" color={palette.neutral.dark}>
+                        No Post
+                    </Typography>
+                </Box>
+            )}
         </>
     );
 };
